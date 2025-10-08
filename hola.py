dee596b8a1f345ab749a07f0ecf42e5b46bb262a
Belén <anabelen.fierensbayo@gmail.com>
2025-10-08 16:19:45 -0300

    Reemplazando hola.py con script de prueba para Actions

--- hola.py
+++ hola.py
@@ -1,7 +1,3 @@
-# # Registro de Claves Seguras
-import os
-user_key = os.getenv('USER_KEY', 'default_clave')
-with open("user_key.txt", "w") as file:
-    file.write(user_key)
-print(f"Clave '{user_key}' guardada exitosamente en user_key.txt")
-print("Prueba de GitHub Actions")print('Nueva prueba')
+# Script de prueba para GitHub Actions
+message = "¡Hola desde GitHub Actions!"
+print(message)
