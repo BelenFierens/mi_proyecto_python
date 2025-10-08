d6a43840c968dd293f3146cbf1095714e4cab8b9
Belén <anabelen.fierensbayo@gmail.com>
2025-10-08 16:00:34 -0300

    Añadiendo prueba para Actions

--- hola.py
+++ hola.py
@@ -4,4 +4,4 @@ user_key = os.getenv('USER_KEY', 'default_clave')
 with open("user_key.txt", "w") as file:
     file.write(user_key)
 print(f"Clave '{user_key}' guardada exitosamente en user_key.txt")
-print("Prueba de GitHub Actions")
+print("Prueba de GitHub Actions")print('Nueva prueba')
