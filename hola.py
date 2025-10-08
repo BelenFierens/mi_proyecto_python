ed985e3d7f46efe248eb9b6c37b90ff420fc339d
Belén <anabelen.fierensbayo@gmail.com>
2025-10-08 14:54:41 -0300

    Adaptado hola.py para GitHub Actions con os.getenv

--- hola.py
+++ hola.py
@@ -1,2 +1,7 @@
-
+# Registro de Claves Seguras
+import os
+user_key = os.getenv('USER_KEY', 'default_clave')
+with open("user_key.txt", "w") as file:
+    file.write(user_key)
+print(f"Clave '{user_key}' guardada exitosamente en user_key.txt")
 print("Prueba de GitHub Actions")
