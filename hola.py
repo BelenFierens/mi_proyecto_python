9784c670951778be6e5c3922cb0c0b8fa5824cef
Belén <anabelen.fierensbayo@gmail.com>
2025-10-07 23:25:55 -0300

    Añadido prueba 1

--- hola.py
+++ hola.py
@@ -3,3 +3,5 @@ user_key = input("Ingresa tu clave personal: ")
 with open("user_key.txt", "w") as file:
     file.write(user_key)
 print(f"Clave '{user_key}' guardada exitosamente en user_key.txt")
+
+print("Prueba 1")
